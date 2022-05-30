ffe0d420713d6dbd6cc938ea695ff61f89a92b0d
Joshwawo <L17330479@hermosillo.tecnm.mx>
2022-05-30 01:57:40 -0700

    cristo rey en ti confio

--- app.js
+++ app.js
@@ -8,7 +8,8 @@ const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(bodyParser.json());
-app.use( bodyParser.urlencoded({ extended: true }) );
+// app.use( bodyParser.urlencoded({ extended: true }) );
+app.use(express.urlencoded({extended:true}))
 app.use(cors());
 
 //Mysql
